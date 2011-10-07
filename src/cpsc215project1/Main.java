6c3b7d1f7e015517a62c880fd460d74198277797
Antonio Malvagomes <amalvag@clemson.edu>
2011-10-07 00:46:43 -0400

    Added XOM library and began writing tests for GameReader.  NOTE: Pathname is currently absolute.  Change to suit your environment.

--- src/cpsc215project1/Main.java
+++ src/cpsc215project1/Main.java
@@ -10,7 +10,14 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+	/*
+	*  TEST GameReader
+	*/
+	GameReader game = new GameReader("/home/amalvag/NetBeansProjects/CPSC215Project1/src/cpsc215project1/savegame");
+	System.out.println(game.getString());
+	/*
+	*  END GameReader test
+	*/
     }
 
 }
