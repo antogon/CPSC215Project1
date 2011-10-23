89294285d4498b67e81d0bf89dd2ce17ea1fd9f1
Patrick Logan <plogan@g.clemson.edu>
2011-10-23 17:04:35 -0400

    Inventory strategy and location, for real this time.

--- src/cpsc215project1/InventoryStrategy.java
+++ src/cpsc215project1/InventoryStrategy.java
@@ -4,10 +4,24 @@
  */
 package cpsc215project1;
 
+import edu.clemson.cs.hamptos.adventure.AdventureCommand;
+import edu.clemson.cs.hamptos.adventure.AdventureEngine;
+import edu.clemson.cs.hamptos.adventure.AdventureTarget;
+import edu.clemson.cs.hamptos.adventure.AdventureWindow;
+import edu.clemson.cs.hamptos.adventure.VerbStrategy;
+
 /**
  *
  * @author toiletplumber
  */
-public class InventoryStrategy {
-    
+public class InventoryStrategy implements VerbStrategy{
+
+    public void doCommand(AdventureCommand c, AdventureEngine e, AdventureWindow w) {
+        w.println("Player Inventory");
+        w.println("******************************");
+        
+        for(AdventureTarget t : e.getPlayerInventory()){
+            w.println(t.getShortDescription());
+        }
+    }
 }
