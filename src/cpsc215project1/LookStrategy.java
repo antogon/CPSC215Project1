48bfe2af848aea347dac409eef616dfb515306f4
John Gibbs Dickinson <jgdicki@gecko11.cs.clemson.edu>
2011-10-23 18:25:31 -0400

    Updated LookStrategy to correctly display the items in the room and added proper English in the current savegame filedescriptions

--- src/cpsc215project1/LookStrategy.java
+++ src/cpsc215project1/LookStrategy.java
@@ -15,11 +15,34 @@ public class LookStrategy implements VerbStrategy{
     public void doCommand(AdventureCommand c, AdventureEngine e, AdventureWindow w) {
 		w.println(e.getPlayerLocation().getDescription());
 		String output = "You can see ";
+                int size = e.getPlayerLocation().getLocalTargets().size();
+                int ndx = 0;
+
         for(AdventureTarget t : e.getPlayerLocation().getLocalTargets())
         {
-			output += t.getShortDescription() + ", ";
+            if (size > 2){
+               if (ndx < size - 1){
+                   output += t.getShortDescription() + ", ";
+                   ndx++;
+               }
+               else{
+               output += "and " + t.getShortDescription() + ".";
+               }
+            }
+            else if(size == 1){
+                output += t.getShortDescription() + ".";
+            }
+            else{
+                if(ndx == 0){
+                   output += t.getShortDescription() + " and ";
+                   ndx++;
+                }
+                else{
+                    output += t.getShortDescription() + ".";
+                }
+            }
         }
         w.println(output);
     }
-    
+
 }
