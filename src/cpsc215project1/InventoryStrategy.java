1ed1b7b199a9be7b4f9c16ebe14dfbcec86c5937
Patrick Logan <plogan@g.clemson.edu>
2011-10-23 16:48:06 -0400

    Inventory Strategy, Must be implemented in Location.java

---
+++ src/cpsc215project1/InventoryStrategy.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpsc215project1;
+
+/**
+ *
+ * @author toiletplumber
+ */
+public class InventoryStrategy {
+    
+}
